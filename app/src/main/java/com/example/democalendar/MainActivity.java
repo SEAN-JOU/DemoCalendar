6314804d2133bd52e3f5fa62e40a0e7759185a9a
sean <present810209@gmail.com>
2020-11-20 14:56:52 +0800

    放入calendar

--- app/src/main/java/com/example/democalendar/MainActivity.java
+++ app/src/main/java/com/example/democalendar/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.haibin.calendarview.library.Calendar;
+import com.haibin.calendarview.library.CalendarView;
+
+public class MainActivity extends AppCompatActivity implements
+        CalendarView.OnCalendarSelectListener,
+        CalendarView.OnYearChangeListener,
+        CalendarView.OnMonthChangeListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onYearChange(int year) {
+
+    }
+
+    @Override
+    public void onMonthChange(int year, int month) {
+
+    }
+
+    @Override
+    public void onCalendarOutOfRange(Calendar calendar) {
+
+    }
+
+    @Override
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+
     }
 }
